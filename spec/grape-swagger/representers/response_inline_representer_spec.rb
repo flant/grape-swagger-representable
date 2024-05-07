d2ed3cf8ff948c2971c45f36bbd92d7b46e7646b
Alexander Popov <alex.wayfer@gmail.com>
2024-05-07 16:37:48 +0300

    Replace `require 'spec_helper'` with RSpec option in config

--- spec/grape-swagger/representers/response_inline_representer_spec.rb
+++ spec/grape-swagger/representers/response_inline_representer_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-
 describe 'responseInlineModel' do
   before :all do
     module ThisInlineApi
--- spec/grape-swagger/representers/response_representer_spec.rb
+++ spec/grape-swagger/representers/response_representer_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-
 describe 'responseModel' do
   before :all do
     module ThisApi
